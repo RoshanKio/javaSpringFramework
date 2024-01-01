1b933831c3ec0ee2686318b8a8274eb82e74d831
Ram <ram@krishna.com>
2024-01-01 02:17:30 -0500

    msg

---
+++ spring_fw_1/src/main/java/com/takeo/Hello.java
@@ -0,0 +1,15 @@
+package com.takeo;
+
+public class Hello {
+	
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+}
